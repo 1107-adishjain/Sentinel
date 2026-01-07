b0a1663af30fd3a355a082aad0bb8c113b8edcc1
ADISH <adishjain1107@gmail.com>
2026-01-07 12:09:48 +0530

    Adeded CORS and security Headers middleware to protect from common Web attacks
    Signed-off-by: ADISH <adishjain1107@gmail.com>

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -3,10 +3,21 @@ package routes
 import (
 	cont "github.com/1107-adishjain/sentinel/pkg/controllers"
 	"github.com/gin-gonic/gin"
+	mw "github.com/1107-adishjain/sentinel/pkg/middleware"
+	"github.com/gin-contrib/cors"
 )
 
 func Routes() *gin.Engine{
 	router:= gin.Default()
+
+	router.Use(mw.SecurityHeaders())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowHeaders:  []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		ExposeHeaders: []string{"Content-Length"},
+		MaxAge:        3600,
+	}))
 
 
 	r:=router.Group("/api/v1")
