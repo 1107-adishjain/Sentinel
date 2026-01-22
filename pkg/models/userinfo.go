d41f7b62cee3f466e7940f1a7a73a1cab5698f46
ADISH <adishjain1107@gmail.com>
2026-01-22 19:45:24 +0530

    testing done
    Signed-off-by: ADISH <adishjain1107@gmail.com>

--- pkg/models/userinfo.go
+++ pkg/models/userinfo.go
@@ -1,15 +1,15 @@
 package models
 
-import(
-	"time"
+import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type User struct {
-    Id string `gorm:"column:id;type:uuid;primaryKey"`
-    Email string `gorm:"column:email;type:varchar(100);uniqueIndex;not null"`
-    PasswordHash string `gorm:"column:password_hash;type:varchar(255);not null"`
-    CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
+	Id           string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Email        string    `gorm:"column:email;type:varchar(100);uniqueIndex;not null"`
+	PasswordHash string    `gorm:"column:password_hash;type:varchar(255);not null"`
+	CreatedAt    time.Time `gorm:"column:created_at;autoCreateTime"`
 }
 
 func MigrateUser(db *gorm.DB) error {
