b59f8142586ea9801b61c1e9cb775e203efff77a
ADISH <adishjain1107@gmail.com>
2026-01-22 18:50:35 +0530

    completed jwt auth: testing still reamins
    Signed-off-by: ADISH <adishjain1107@gmail.com>

--- pkg/models/userinfo.go
+++ pkg/models/userinfo.go
@@ -6,7 +6,7 @@ import(
 )
 
 type User struct {
-    Id string `gorm:"column:id;type:uuid;default:uuid_generate_v4();primaryKey"`
+    Id string `gorm:"column:id;type:uuid;primaryKey"`
     Email string `gorm:"column:email;type:varchar(100);uniqueIndex;not null"`
     PasswordHash string `gorm:"column:password_hash;type:varchar(255);not null"`
     CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
